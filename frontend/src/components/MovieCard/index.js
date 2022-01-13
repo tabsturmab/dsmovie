600733704da0bc4cd0ef7bdc4d22d1562425a0a9
Andre Silva <tadsturmab2014@gmail.com>
2022-01-13 17:03:20 -0300

    Correction movieCard

--- frontend/src/components/MovieCard/index.js
+++ frontend/src/components/MovieCard/index.js
@@ -17,7 +17,7 @@ function MovieCard() {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <Link to={'/form/${mobie.id}'}>
+                <Link to={`/form/${movie.id}!`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
             </div>
